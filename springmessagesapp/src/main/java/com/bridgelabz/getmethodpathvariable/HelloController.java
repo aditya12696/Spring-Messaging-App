e7082d9b20daf7edc6355168c3e0539c16f3fb01
aditya12696 <aditysharma127@gmail.com>
2025-02-21 23:15:57 +0530

    Aditya Add program for get method using path variable in springboot

---
+++ springmessagesapp/src/main/java/com/bridgelabz/getmethodpathvariable/HelloController.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.getmethodpathvariable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    // GET Request with Path Variable
+    @GetMapping("/param/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+}
